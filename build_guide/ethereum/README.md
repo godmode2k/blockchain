da07404996081da292861de8eccb1b370bc8751b
godmode2k <godmode2k@hotmail.com>
2022-02-03 23:28:27 +0900

    ERC-20 updated

--- build_guide/ethereum/README.md
+++ build_guide/ethereum/README.md
@@ -1,4 +1,39 @@
-Ethereum ERC-20 Token create & deploy test
+Ethereum Token create & deploy test
 ----------
-erc20_deploy_tutorial.txt
+> ERC-20: erc20_deploy_tutorial.txt </br>
+
+
+Environment
+----------
+> build all and tested on GNU/Linux
+
+    GNU/Linux: Ubuntu 20.04_x64 LTS
+    Ethereum: Geth/v1.10.15-stable/linux-amd64/go1.10.15
+    Golang: go1.15.5 linux/amd64
+    Node.js: node-v16.13.2
+    Network: Ethereum Local Private Network
+
+
+Tools installation
+----------
+```sh
+Golang
+$ wget https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz
+$ tar xzvf go1.15.5.linux-amd64.tar.gz -C /usr/local/
+$ echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+
+Ethereum
+$ git clone https://github.com/ethereum/go-ethereum.git -b v1.10.15
+$ cd go-ethereum-1.10.15 && make all
+$ cd .. && ln -s go-ethereum-1.10.15/build/bin/geth .
+```
+
+
+Run
+----------
+```sh
+ERC-20
+$ yes "" | sh ./new_token.sh [Token Name] [Token Symbol] [Token Decimals] [Token Total Supply]
+e.g., yes "" | sh new_token.sh ABCToken ABC 18 10000
+```
 
--- build_guide/ethereum/new_token.sh
+++ build_guide/ethereum/new_token.sh
@@ -29,11 +29,11 @@
 # Note:
 # -----------------------------------------------------------------
 # 1. nodejs
-#     $ wget https://nodejs.org/dist/v10.16.2/node-v10.16.2-linux-x64.tar.xz
-#     $ tar xJvf node-v10.16.2-linux-x64.tar.xz
-#     $ export PATH=`pwd`/node-v10.16.2-linux-x64/bin:$PATH
-#     $ echo export PATH=`pwd`/node-v10.16.2-linux-x64/bin:$PATH >> $HOME/.profile
-#     $ echo export PATH=`pwd`/node-v10.16.2-linux-x64/bin:$PATH >> $HOME/.bashrc
+#     $ wget https://nodejs.org/dist/v16.13.2/node-v16.13.2-linux-x64.tar.xz
+#     $ tar xJvf node-v16.13.2-linux-x64.tar.xz
+#     $ export PATH=`pwd`/node-v16.13.2-linux-x64/bin:$PATH
+#     $ echo export PATH=`pwd`/node-v16.13.2-linux-x64/bin:$PATH >> $HOME/.profile
+#     $ echo export PATH=`pwd`/node-v16.13.2-linux-x64/bin:$PATH >> $HOME/.bashrc
 #
 # 2. private-key
 #    // export private-key from keystore file
@@ -55,6 +55,7 @@
 #
 # 3. run
 #     $ yes "" | sh ./new_token.sh [Token Name] [Token Symbol] [Token Decimals] [Token Total Supply]
+#     e.g., yes "" | sh new_token.sh ABCToken ABC 18 10000
 # -----------------------------------------------------------------
 #
 
@@ -66,8 +67,8 @@ TOKEN_DECIMALS=$3
 TOKEN_TOTAL_SUPPLY=$4
 
 if [ -z $TOKEN_NAME ] || [ -z $TOKEN_SYMBOL ] || [ -z $TOKEN_DECIMALS ] || [ -z $TOKEN_TOTAL_SUPPLY ]; then
-	echo "Usage: $ yes \"\" | sh $0 [Token Name] [Token Symbol] [Token Decimals] [Token Total Supply]";
-	exit
+    echo "Usage: $ yes \"\" | sh $0 [Token Name] [Token Symbol] [Token Decimals] [Token Total Supply]";
+    exit
 fi
 #echo "TOKEN NAME = $TOKEN_NAME"
 #echo "TOKEN SYMBOL = $TOKEN_SYMBOL"
@@ -79,29 +80,31 @@ fi
 #TOKEN_SYMBOL="TTA"
 #TOKEN_DECIMALS="18"
 #TOKEN_TOTAL_SUPPLY="1000000"
-OWNER_PRIVATE_KEY="<Owner Private-key>" # SEE 'Note' above for how to export private-key from keystore
-NODE_HOST="http://localhost:8545/"
+OWNER_PRIVATE_KEY="7158d312bd2fa38c4de3da43f7bc107bf116117685f43ddcb597a9310b3ca5b8"
+#NODE_HOST="http://localhost:8545/"
+NODE_HOST="http://localhost:8544/"
 WORK_DIR="/eth_erc20"
 TOKEN_DIR="tokens/$TOKEN_NAME"
 
 
 if [ ! -d "$WORK_DIR" ]; then
-	echo "$WORK_DIR doesn't existed"
-	exit
+    echo "$WORK_DIR doesn't existed"
+    exit
 fi
 
 #if [ ! -d "$WORK_DIR/tokens" ]; then
-#	echo "create $WORK_DIR/token"
-#	mkdir -p "$WORK_DIR/tokens"
+#   echo "create $WORK_DIR/token"
+#   mkdir -p "$WORK_DIR/tokens"
 #fi
 
 cd $WORK_DIR
 if [ -d "$TOKEN_DIR" ]; then
-	echo "existed token"
-	exit
+    echo "existed token"
+    exit
 fi
 
-export PATH=/eth_erc20/node-v10.16.2-linux-x64/bin:$PATH
+#export PATH=/eth_erc20/node-v10.16.2-linux-x64/bin:$PATH
+#export PATH=/work/node-v16.13.2-linux-x6/bin:$PATH
 
 mkdir -p "$TOKEN_DIR" &&
 cd "$TOKEN_DIR" &&
@@ -109,9 +112,24 @@ cd "$TOKEN_DIR" &&
 
 # No install 'truffle' always
 #npm install --unsafe-perm=true --allow-root -g truffle@4.1.15 &&
+#npm install --unsafe-perm=true --allow-root -g truffle &&
+#npm install --unsafe-perm=true --allow-root -g truffle@5.3.0 && --save
+#
+# npm uninstall -g truffle
+# npm install -g truffle
+#
+#
+# Error:  *** Deployment Failed ***
+# "Migrations" -- Web3ProviderEngine does not support synchronous requests..
+#
+# USE THIS (2022.02.02 currently) if errors occur like above.
+# npm install --unsafe-perm=true --allow-root -g truffle@5.3.0
+#
 truffle init --yes &&
 npm init &&
-npm install zeppelin-solidity &&
+#npm install zeppelin-solidity &&
+#npm install openzeppelin-solidity &&
+npm install @openzeppelin/contracts &&
 
 npm install --unsafe-perm=true --allow-root truffle-wallet-provider ethereumjs-wallet@0.6.4 &&
 npm install --unsafe-perm=true --allow-root web3 &&
@@ -119,23 +137,43 @@ npm install --unsafe-perm=true --allow-root web3 &&
 
 echo ">>> contracts/$TOKEN_NAME.sol"
 cat << EOF >> contracts/$TOKEN_NAME.sol &&
+/*
+// ERC-20: v0.4.x
 pragma solidity ^0.4.18;
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract $TOKEN_NAME is StandardToken, Ownable {
-	string public name = "$TOKEN_NAME"; 
-	string public symbol = "$TOKEN_SYMBOL";
-	uint public decimals = $TOKEN_DECIMALS;
-	//uint public INITIAL_SUPPLY = 1000000 * (10 ** (uint256)decimals);
-	uint public INITIAL_SUPPLY = $TOKEN_TOTAL_SUPPLY * (10 ** decimals);
+    string public name = "$TOKEN_NAME"; 
+    string public symbol = "$TOKEN_SYMBOL";
+    uint public decimals = $TOKEN_DECIMALS;
+    //uint public INITIAL_SUPPLY = 1000000 * (10 ** (uint256)decimals);
+    uint public INITIAL_SUPPLY = $TOKEN_TOTAL_SUPPLY * (10 ** decimals);
 
-	function $TOKEN_NAME() public {
-		totalSupply_ = INITIAL_SUPPLY;
-		owner = msg.sender;
-		balances[msg.sender] = INITIAL_SUPPLY;
-		emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
-	}
+    function $TOKEN_NAME() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        owner = msg.sender;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+    }
+}
+*/
+
+// ERC-20: v0.8.x
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ABCToken is ERC20 {
+    string public __name = "$TOKEN_NAME"; 
+    string public __symbol = "$TOKEN_SYMBOL";
+    uint public __decimals = $TOKEN_DECIMALS;
+    //uint public INITIAL_SUPPLY = 1000000 * (10 ** (uint256)decimals);
+    uint public __INITIAL_SUPPLY = $TOKEN_TOTAL_SUPPLY * (10 ** __decimals);
+
+    constructor() ERC20(__name, __symbol) {
+        _mint(msg.sender, __INITIAL_SUPPLY);
+        emit Transfer(address(0), msg.sender, __INITIAL_SUPPLY);
+    }
 }
 EOF
 
@@ -144,7 +182,7 @@ echo ">>> migrations/2_deploy_$TOKEN_NAME.js"
 cat << EOF >> migrations/2_deploy_$TOKEN_NAME.js &&
 var $TOKEN_NAME = artifacts.require("./$TOKEN_NAME.sol");
 module.exports = function(deployer) {
-	deployer.deploy($TOKEN_NAME);
+    deployer.deploy($TOKEN_NAME);
 };
 EOF
 
@@ -166,7 +204,7 @@ const Wallet = require('ethereumjs-wallet');
 //const thirdparty = require('ethereumjs-wallet/thirdparty');
 
 const _input = '{"address":"","crypto":""...,"version":3}';
-const _password = "<passphrase>";
+const _password = "12345678";
 
 const mywallet = Wallet.fromV3(_input, _password);
 const privatekey = mywallet.getPrivateKey();
@@ -190,39 +228,47 @@ var mainnet_provider = new WalletProvider(mainnet_wallet, "http://localhost:8545
 
 
 module.exports = {
-	networks: {
-		development: {
-			host: "localhost",
-			port: 8545,
-			network_id: "*" // Match any network id
-		},
-		localtest: {
-			provider: localtest_provider,
-			//from: "0x...",
-			gas: 4600000,
+    compilers: {
+        solc: {
+        //version: "0.4.18",
+        version: "0.8.2",
+        }
+    },
 
-			// web3 version < v1.0.0; v0.18.3 here
-			//gasPrice: web3.toWei("20", "gwei"),
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+        localtest: {
+            provider: localtest_provider,
+            //from: "0x...",
+            gas: 4600000,
 
-			// web3 version > v1.0.0
-			gasPrice: web3.utils.toWei("20", "gwei"),
+            // web3 version < v1.0.0; v0.18.3 here
+            //gasPrice: web3.toWei("20", "gwei"),
 
-			//network_id: "*",
-			network_id: "1000",
-		}
+            // web3 version > v1.0.0
+            gasPrice: web3.utils.toWei("20", "gwei"),
+
+            //network_id: "*",
+            network_id: "1000998877",
+        }
 /*,
-		mainnet: {
-			provider: mainnet_provider,
-			gas: 4600000,
-			gasPrice: web3.toWei("20", "gwei"),
-			network_id: "1",
-		}
+        mainnet: {
+            provider: mainnet_provider,
+            gas: 4600000,
+            gasPrice: web3.toWei("20", "gwei"),
+            network_id: "1",
+        }
 */
-	}
+    }
 };
 EOF
 
 
+mv truffle-config.js truffle-config.js.orig &&
 truffle compile &&
 truffle deploy --network localtest
 
